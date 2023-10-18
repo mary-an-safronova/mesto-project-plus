27600a138c12e4dff37ec47f29f9fc62b82974ca
Mariia Safronova <mary.an.safronova@yandex.ru>
2023-10-18 18:22:39 +0300

    fix: the controller code has been optimized, the decorator function has been added when updating user data

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -3,10 +3,19 @@ import mongoose from 'mongoose';
 import User from '../models/user';
 import { STATUS_CODE, ERROR_MESSAGE } from '../utils/constants/errors';
 
+const sendUserResponse = (user: any, res: Response) => {
+  res.send({
+    name: user?.name,
+    about: user?.about,
+    avatar: user?.avatar,
+    _id: user?._id,
+  });
+};
+
 // Получение всех пользовтелей
-export const getUsers = (req: Request, res: Response) => User.find({})
-  .select('name about avatar _id') // Поля, включенные в результат ответа
-  .then((users) => {
+export const getUsers = async (req: Request, res: Response) => {
+  try {
+    const users = await User.find({}).select('name about avatar _id'); // Поля, включенные в результат ответа
     const updatedUsers = users.map((user: any) => {
       const updatedUser = {
         name: user?.name,
@@ -17,105 +26,85 @@ export const getUsers = (req: Request, res: Response) => User.find({})
       return updatedUser;
     });
     res.send(updatedUsers);
-  })
-  .catch(() => res.status(STATUS_CODE.InternalServerError)
-    .send({ message: ERROR_MESSAGE.Error }));
+  } catch (err) {
+    res.status(STATUS_CODE.InternalServerError).send({ message: ERROR_MESSAGE.Error });
+  }
+};
 
 // Получение одного пользователя по id
-export const getUser = (req: Request, res: Response) => {
-  const { userId } = req.params;
-
-  return User
-    .findById(userId)
-    .orFail()
-    .select('name about avatar _id') // Поля, включенные в результат ответа
-    .then((user) => {
-      res.send({
-        name: user?.name,
-        about: user?.about,
-        avatar: user?.avatar,
-        _id: user?._id,
-      });
-    })
-    .catch((err) => {
-      if (err instanceof mongoose.Error.CastError) {
-        res.status(STATUS_CODE.BadRequest).send({ message: ERROR_MESSAGE.IncorrectId });
-      } else if (err instanceof mongoose.Error.DocumentNotFoundError) {
-        res.status(STATUS_CODE.NotFound).send({ message: ERROR_MESSAGE.UserNotFound });
-      } else {
-        res.status(STATUS_CODE.InternalServerError).send({ message: ERROR_MESSAGE.Error });
-      }
-    });
+export const getUser = async (req: Request, res: Response) => {
+  try {
+    const { userId } = req.params;
+    const user = await User.findById(userId).orFail().select('name about avatar _id'); // Поля, включенные в результат ответа
+    sendUserResponse(user, res);
+  } catch (err) {
+    if (err instanceof mongoose.Error.CastError) {
+      res.status(STATUS_CODE.BadRequest).send({ message: ERROR_MESSAGE.IncorrectId });
+    } else if (err instanceof mongoose.Error.DocumentNotFoundError) {
+      res.status(STATUS_CODE.NotFound).send({ message: ERROR_MESSAGE.UserNotFound });
+    } else {
+      res.status(STATUS_CODE.InternalServerError).send({ message: ERROR_MESSAGE.Error });
+    }
+  }
 };
 
 // Создание нового пользователя
-export const createUser = (req: Request, res: Response) => {
-  const { name, about, avatar } = req.body;
+export const createUser = async (req: Request, res: Response) => {
+  try {
+    const { name, about, avatar } = req.body;
+    const user = await User.create({ name, about, avatar });
+    res.status(STATUS_CODE.Created).send(user);
+  } catch (err) {
+    if (err instanceof mongoose.Error.ValidationError) {
+      res.status(STATUS_CODE.BadRequest).send({ message: err.message });
+    } else {
+      res.status(STATUS_CODE.InternalServerError).send({ message: ERROR_MESSAGE.Error });
+    }
+  }
+};
 
-  User
-    .create({ name, about, avatar })
-    .then((user) => res.status(STATUS_CODE.Created).send(user))
-    .catch((err) => {
-      if (err instanceof mongoose.Error.ValidationError) {
-        res.status(STATUS_CODE.BadRequest).send({ message: err.message });
-      } else {
-        res.status(STATUS_CODE.InternalServerError).send({ message: err.message });
-      }
-    });
+// Функция-декоратор для вынесения общих ошибок при обновлении данных пользователя
+const handleUpdateUserErrors = (fn: any) => async (
+  req: Request,
+  res: Response,
+) => {
+  try {
+    await fn(req, res);
+  } catch (err) {
+    if (err instanceof mongoose.Error.ValidationError) {
+      res.status(STATUS_CODE.BadRequest).send({ message: err.message });
+    } else if (err instanceof mongoose.Error.DocumentNotFoundError) {
+      res.status(STATUS_CODE.NotFound).send({ message: ERROR_MESSAGE.UserNotFound });
+    } else {
+      res.status(STATUS_CODE.InternalServerError).send({ message: ERROR_MESSAGE.Error });
+    }
+  }
+};
+
+// Общая логика изменения значений полей пользователя
+const updateUserRequest = async (req: Request, res: Response, fields: any) => {
+  const userId = req.user._id;
+
+  const user = await User
+    .findByIdAndUpdate(userId, fields, { new: true, runValidators: true })
+    .orFail()
+    .select('name about avatar _id'); // Поля, включенные в результат ответа
+
+  sendUserResponse(user, res);
 };
 
 // Изменение значений полей name и about пользователя
-export const updateUserInfo = (req: Request, res: Response) => {
+const updateUserInfo = async (req: Request, res: Response) => {
   const { name, about } = req.body;
-  const userId = req.user._id;
-
-  return User
-    .findByIdAndUpdate(userId, { name, about }, { new: true, runValidators: true })
-    .orFail()
-    .select('name about avatar _id') // Поля, включенные в результат ответа
-    .then((user) => {
-      res.send({
-        name: user?.name,
-        about: user?.about,
-        avatar: user?.avatar,
-        _id: user?._id,
-      });
-    })
-    .catch((err) => {
-      if (err instanceof mongoose.Error.ValidationError) {
-        res.status(STATUS_CODE.BadRequest).send({ message: err.message });
-      } else if (err instanceof mongoose.Error.DocumentNotFoundError) {
-        res.status(STATUS_CODE.NotFound).send({ message: ERROR_MESSAGE.UserNotFound });
-      } else {
-        res.status(STATUS_CODE.InternalServerError).send({ message: err.message });
-      }
-    });
+  await updateUserRequest(req, res, { name, about });
 };
+
+export const updateUserInfoController = handleUpdateUserErrors(updateUserInfo);
 
 // Изменение значения поля avatar пользователя
-export const updateUserAvatar = (req: Request, res: Response) => {
+const updateUserAvatar = async (req: Request, res: Response) => {
   const { avatar } = req.body;
-  const userId = req.user._id;
-
-  return User
-    .findByIdAndUpdate(userId, { avatar }, { new: true, runValidators: true })
-    .orFail()
-    .select('name about avatar _id') // Поля, включенные в результат ответа
-    .then((user) => {
-      res.send({
-        name: user?.name,
-        about: user?.about,
-        avatar: user?.avatar,
-        _id: user?._id,
-      });
-    })
-    .catch((err) => {
-      if (err instanceof mongoose.Error.ValidationError) {
-        res.status(STATUS_CODE.BadRequest).send({ message: err.message });
-      } else if (err instanceof mongoose.Error.DocumentNotFoundError) {
-        res.status(STATUS_CODE.NotFound).send({ message: ERROR_MESSAGE.UserNotFound });
-      } else {
-        res.status(STATUS_CODE.InternalServerError).send({ message: err.message });
-      }
-    });
+  await updateUserRequest(req, res, { avatar });
 };
+
+export const updateUserAvatarController = handleUpdateUserErrors(updateUserAvatar);
