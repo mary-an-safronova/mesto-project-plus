30c71d330a33fe4900dc63986d5c4a49cdb8f4e9
Mariia Safronova <mary.an.safronova@yandex.ru>
2023-10-21 17:59:47 +0300

    fix: The route of the unknown route has been moved above the logger, the error has been passed to next

--- src/middlewares/centralized-error.ts
+++ src/middlewares/centralized-error.ts
@@ -2,12 +2,14 @@ import { NextFunction, Request, Response } from 'express';
 import mongoose from 'mongoose';
 import jwt from 'jsonwebtoken';
 import { STATUS_CODE, ERROR_MESSAGE } from '../utils/constants/errors';
+import { NotFoundError } from '../utils/errors';
 
 // Middleware для централизованной обработки ошибок
 export default (err: any, req: Request, res: Response, next: NextFunction) => {
   // Определение статуса и сообщения об ошибке
   let statusCode = STATUS_CODE.InternalServerError; // Внутренняя ошибка сервера
   let errorMessage = ERROR_MESSAGE.Error;
+  const status = err.statusCode;
 
   if (err instanceof mongoose.Error.ValidationError) {
     statusCode = STATUS_CODE.BadRequest; // Плохой запрос
@@ -19,14 +21,17 @@ export default (err: any, req: Request, res: Response, next: NextFunction) => {
     statusCode = STATUS_CODE.BadRequest; // Некорректные данные
     errorMessage = ERROR_MESSAGE.IncorrectData;
   } else if (err instanceof jwt.JsonWebTokenError) {
-    statusCode = STATUS_CODE.Unauthorized; // Неавторизовано
+    statusCode = STATUS_CODE.Unauthorized; // Неавторизован
     errorMessage = ERROR_MESSAGE.AuthenticationError;
   } else if (err.code === 11000) {
     statusCode = STATUS_CODE.Conflict; // Почта уже есть в базе
     errorMessage = ERROR_MESSAGE.MailAlreadyExists;
   } else if (err.message === ERROR_MESSAGE.IncorrectEmailOrPassword) {
-    statusCode = STATUS_CODE.Unauthorized; // Плохой запрос
+    statusCode = STATUS_CODE.Unauthorized; // Неавторизован
     errorMessage = ERROR_MESSAGE.IncorrectEmailOrPassword;
+  } else if (err instanceof Error && status === STATUS_CODE.NotFound) {
+    statusCode = STATUS_CODE.NotFound; // Маршрут не найден
+    errorMessage = ERROR_MESSAGE.NotFoundRoute;
   }
 
   // Отправка ответа с соответствующим статусом и сообщением об ошибке
--- src/utils/errors/index.ts
+++ src/utils/errors/index.ts
@@ -1,7 +1,9 @@
 import BadRequestError from './bad-request-err';
 import UnauthorizedError from './unauthorized-error';
+import NotFoundError from './not-found-error';
 
 export {
   BadRequestError,
   UnauthorizedError,
+  NotFoundError,
 };
